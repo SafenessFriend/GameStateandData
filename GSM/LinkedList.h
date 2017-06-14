86967bdbcb98b29273d81a4c092d0ad1759a7460
Jack Booker <booker.jack@gmail.com>
2017-06-14 14:36:22 +1000

    linked list update

--- GSM/LinkedList.h
+++ GSM/LinkedList.h
@@ -1,8 +1,22 @@
 #pragma once
+
+
 class LinkedList
+
 {
 public:
 	LinkedList();
 	~LinkedList();
+
+	void createNode(int value);					// create a node
+	void displayNode();							// display a node
+
+	void insertStart(int value);				// insertion at the start
+	void insertPosition(int pos, int value);	// insert at a particular position
+	void deleteFirst();							// delete first node
+	void deleteLast();							// delete last node
+	void deletePosition(int pos);				// delete a certaion position
+private:
+	node *head, *tail;
 };
 
